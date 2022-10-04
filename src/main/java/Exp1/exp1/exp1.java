e7aee831dee722e77aaa2fc9252ed11077f6da14
Eternal4869 <1303498336@qq.com>
2022-10-04 13:37:35 +0800

    更新文件结构

--- src/main/java/Exp1/exp1/exp1.java
+++ src/main/java/Exp1/exp1/exp1.java
@@ -1,4 +1,4 @@
-package exp1;
+package Exp1.exp1;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/Exp1/exp2/exp2.java
+++ src/main/java/Exp1/exp2/exp2.java
@@ -1,4 +1,4 @@
-package exp2;
+package Exp1.exp2;
 
 import java.util.ArrayList;
 import java.util.List;
