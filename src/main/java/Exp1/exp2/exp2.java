44964d7b1ab4ad017aa3cc015cfcade6f01293e4
Eternal4869 <1303498336@qq.com>
2022-10-08 13:20:20 +0800

    modify some errors

--- src/main/java/Exp1/exp2/exp2.java
+++ src/main/java/Exp1/exp2/exp2.java
@@ -16,53 +16,57 @@ public class exp2 {
             weight = in.nextDouble();
             coins.add(weight);
         }
-        System.out.print("假币所在的序号为(序号从0开始) : ");
+        System.out.print("假币所在的序号为(序号从0开始,-1代表硬币数量不足以查询出假币位置) : ");
         counterfeitMoney(coins, 0, coins.size() - 1);
     }
 
     public static void counterfeitMoney(List<Double> coins, int begin, int end) {
-        int mediumNum = begin + (end - begin) / 2;
-        int length = end + 1 - begin;
-        int vacancy = 0;
-        if (length % 2 == 0) {
-            if (length == 2) {
-                if (coins.get(begin) > coins.get(end)) {
-                    System.out.println(end + vacancy);
-                } else {
-                    System.out.println(begin + vacancy);
-                }
-            } else {
-                double coins1Weight = calcWeight(coins, begin, mediumNum);
-                double coins2Weight = calcWeight(coins, mediumNum + 1, end);
-                if (coins1Weight > coins2Weight) {
-                    counterfeitMoney(coins, mediumNum + 1, end);
-                } else {
-                    counterfeitMoney(coins, begin, mediumNum);
-                }
-            }
+        if (coins.size() == 1 || coins.size() == 0) {
+            System.out.println("-1");
         } else {
-            if (length == 1) {
-                System.out.println(begin + vacancy);
+            int mediumNum = begin + (end - begin) / 2;
+            int length = end + 1 - begin;
+            if (length % 2 == 0) {
+                if (length == 2) {
+                    if (coins.get(begin) > coins.get(end)) {
+                        System.out.println(end);
+                        System.exit(0);
+                    } else {
+                        System.out.println(begin);
+                        System.exit(0);
+                    }
+                } else {
+                    double coins1Weight = calcWeight(coins, begin, mediumNum);
+                    double coins2Weight = calcWeight(coins, mediumNum + 1, end);
+                    if (coins1Weight > coins2Weight) {
+                        counterfeitMoney(coins, mediumNum + 1, end);
+                    } else {
+                        counterfeitMoney(coins, begin, mediumNum);
+                    }
+                }
             } else {
-                coins.remove(mediumNum);
-                vacancy++;
-                double coins1Weight = calcWeight(coins, begin, mediumNum - 1);
-                double coins2Weight = calcWeight(coins, mediumNum, end - 1);
-                if (coins1Weight == coins2Weight) {
-                    System.out.println(mediumNum);
-                }
-                if (coins1Weight < coins2Weight) {
-                    counterfeitMoney(coins, begin, mediumNum - 1);
-                }
-                if (coins1Weight > coins2Weight) {
-                    counterfeitMoney(coins, mediumNum, end - 1);
+                if (length == 1) {
+                    System.out.println(begin);
+                } else {
+                    double coins1Weight = calcWeight(coins, begin, mediumNum - 1);
+                    double coins2Weight = calcWeight(coins, mediumNum + 1, end);
+                    if (coins1Weight == coins2Weight) {
+                        System.out.println(mediumNum);
+                        System.exit(0);
+                    }
+                    if (coins1Weight < coins2Weight) {
+                        counterfeitMoney(coins, begin, mediumNum - 1);
+                    }
+                    if (coins1Weight > coins2Weight) {
+                        counterfeitMoney(coins, mediumNum + 1, end);
+                    }
                 }
             }
         }
     }
 
     public static double calcWeight(List<Double> coins, int begin, int end) {
-        int result = 0;
+        double result = 0;
         for (int i = begin; i <= end; i++) {
             result += coins.get(i);
         }
