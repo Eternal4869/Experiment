3a22ce11e4904e68945d1f135874181a98c82226
Eternal4869 <1303498336@qq.com>
2022-10-09 11:35:12 +0800

    create files

---
+++ src/main/java/Exp2/exp1/exp1.java
@@ -0,0 +1,7 @@
+package Exp2.exp1;
+
+public class exp1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Exp2/exp2/exp2.java
@@ -0,0 +1,7 @@
+package Exp2.exp2;
+
+public class exp2 {
+    public static void main(String[] args) {
+
+    }
+}
