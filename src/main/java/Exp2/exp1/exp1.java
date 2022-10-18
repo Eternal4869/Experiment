ccde71736690b4871ce53c4086fbf1016ac23509
Eternal4869 <1303498336@qq.com>
2022-10-18 17:37:05 +0800

    update Exp2/exp1/exp1

--- src/main/java/Exp2/exp1/exp1.java
+++ src/main/java/Exp2/exp1/exp1.java
@@ -10,27 +10,30 @@ public class exp1 {
         Scanner in = new Scanner(System.in);
         System.out.print("请输入挂钩数量 : ");
         int n = in.nextInt();
-        System.out.print("请输入钩码质量 : ");
-        for (int i = 0; i < n; i++) {
-            int t = in.nextInt();
-            weights.add(t);
-        }
-        int m = 0;
-        for (int i = 0; i < n; i++) {
-            m += weights.get(i);
-        }
-        m /= 2;
-        int[] h = new int[m + 1];
-        h[0] = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = m; j >= 1; j--) {
-                if (weights.get(i) <= j) {
-                    h[j] = h[j] + h[j - weights.get(i)];
+        if (n <= 0) System.out.println("数量有误，请重新输入");
+        else {
+            System.out.print("请输入钩码质量 : ");
+            for (int i = 0; i < n; i++) {
+                int t = in.nextInt();
+                weights.add(t);
+            }
+            int m = 0;
+            for (int i = 0; i < n; i++) {
+                m += weights.get(i);
+            }
+            m /= 2;
+            int[] h = new int[m + 1];
+            h[0] = 1;
+            for (int i = 0; i < n; i++) {
+                for (int j = m; j >= 1; j--) {
+                    if (weights.get(i) <= j) {
+                        h[j] = h[j] + h[j - weights.get(i)];
+                    }
                 }
             }
-        }
-        for (int i = 0; i < h.length; i++) {
-            System.out.print(h[i] + " ");
+            for (int i = 0; i < h.length; i++) {
+                System.out.print(h[i] + " ");
+            }
         }
     }
 }
