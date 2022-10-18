61310d80178934c278ed1d6ce1992153f5ef9f15
Eternal4869 <1303498336@qq.com>
2022-10-18 11:32:58 +0800

    update Exp2/exp1/exp1.java

--- src/main/java/Exp2/exp1/exp1.java
+++ src/main/java/Exp2/exp1/exp1.java
@@ -1,7 +1,36 @@
 package Exp2.exp1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class exp1 {
     public static void main(String[] args) {
-
+        List<Integer> weights = new ArrayList<Integer>();
+        Scanner in = new Scanner(System.in);
+        System.out.print("请输入挂钩数量 : ");
+        int n = in.nextInt();
+        System.out.print("请输入钩码质量 : ");
+        for (int i = 0; i < n; i++) {
+            int t = in.nextInt();
+            weights.add(t);
+        }
+        int m = 0;
+        for (int i = 0; i < n; i++) {
+            m += weights.get(i);
+        }
+        m /= 2;
+        int[] h = new int[m + 1];
+        h[0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = m; j >= 1; j--) {
+                if (weights.get(i) <= j) {
+                    h[j] = h[j] + h[j - weights.get(i)];
+                }
+            }
+        }
+        for (int i = 0; i < h.length; i++) {
+            System.out.print(h[i] + " ");
+        }
     }
 }
