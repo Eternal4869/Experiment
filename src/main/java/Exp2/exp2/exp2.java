05ebee0bacedb495dd7875876ca9bebbfe576c74
Eternal4869 <1303498336@qq.com>
2022-10-18 17:45:18 +0800

    update Exp2/exp2/exp2

--- src/main/java/Exp2/exp2/exp2.java
+++ src/main/java/Exp2/exp2/exp2.java
@@ -9,29 +9,32 @@ public class exp2 {
         System.out.print("请输入数塔高度 : ");
         Scanner in = new Scanner(System.in);
         int height = in.nextInt();
-        List<List<Integer>> tower = new ArrayList<List<Integer>>();
-        for (int i = 0; i < height; i++) {
-            tower.add(new ArrayList<Integer>());
-        }
-        System.out.println("请按照数塔规则输入步数 : ");
-        for (int i = 0; i < height; i++) {
-            for (int i1 = 0; i1 <= i; i1++) {
-                int step = in.nextInt();
-                tower.get(i).add(step);
+        if (height <= 0) System.out.println("高度有误，请重新输入");
+        else {
+            List<List<Integer>> tower = new ArrayList<List<Integer>>();
+            for (int i = 0; i < height; i++) {
+                tower.add(new ArrayList<Integer>());
             }
-        }
-        int i = 0;                                                                  //将tower转换为数组
-        int[][] towerArray = new int[tower.size()][];
-        for (List<Integer> integers : tower) {
-            Object[] objects = integers.toArray();
-            int[][] towerarray = new int[tower.size()][objects.length];
-            for (int j = 0; j < objects.length; j++) {
-                towerarray[i][j] = (int) objects[j];
+            System.out.println("请按照数塔规则输入步数 : ");
+            for (int i = 0; i < height; i++) {
+                for (int i1 = 0; i1 <= i; i1++) {
+                    int step = in.nextInt();
+                    tower.get(i).add(step);
+                }
             }
-            towerArray[i] = towerarray[i];
-            i++;
+            int i = 0;                                                                  //将tower转换为数组
+            int[][] towerArray = new int[tower.size()][];
+            for (List<Integer> integers : tower) {
+                Object[] objects = integers.toArray();
+                int[][] towerarray = new int[tower.size()][objects.length];
+                for (int j = 0; j < objects.length; j++) {
+                    towerarray[i][j] = (int) objects[j];
+                }
+                towerArray[i] = towerarray[i];
+                i++;
+            }
+            towerMaxStep(towerArray);
         }
-        towerMaxStep(towerArray);
     }
 
     public static void towerMaxStep(int[][] tower) {
@@ -56,13 +59,8 @@ public class exp2 {
         for (int i = 1; i < height; i++) {
             for (int i1 = 0; i1 <= i; i1++) {
                 if (towerCopy[i][i1] == maxStep) {
-                    if (i == height - 1){
-                        System.out.print("(" + i + ", " + i1 + ")");
-                    }
-                    else {
-                        System.out.print("(" + i + ", " + i1 + ")->");
-                        maxStep-=tower[i][i1];
-                    }
+                    System.out.print("(" + i + ", " + i1 + ")->");
+                    maxStep -= tower[i][i1];
                 }
             }
         }
